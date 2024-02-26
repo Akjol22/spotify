9da883a6ad45f14136f8bec83d4064511b421619
Akjol22 <118152255+Akjol22@users.noreply.github.com>
2024-02-26 14:34:46 +0600

    Update settings.py
    Email

--- spotify/spotify/settings.py
+++ spotify/spotify/settings.py
@@ -146,4 +146,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'akzolkanaev81@gmail.com'
-EMAIL_HOST_PASSWORD = 'azbblqwcjzaelbrb'
+EMAIL_HOST_PASSWORD = 
